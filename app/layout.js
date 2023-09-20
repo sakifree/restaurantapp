e5d9236b272c6bff81fa14e047a6c15644d4b66b
Sakinah Freeman <freemansakinah@gmail.com>
2023-09-19 21:23:19 -0400

    removes given code

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,4 @@
-import './globals.css'
+// import './globals.css'
 import { Inter } from 'next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -11,6 +11,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <title>Example Text</title>
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   )
